71c583615bf9552dce31377156dac36810bec244
Israel Nieto <isra.utch@gmail.com>
2021-03-14 18:29:56 -0700

    cambio modelo user

--- app/User.php
+++ app/User.php
@@ -4,11 +4,12 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
+use Silber\Bouncer\Database\HasRolesAndAbilities;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, HasRolesAndAbilities;
 
     /**
      * The attributes that are mass assignable.
